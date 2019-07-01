e1cb89970790524fb5d9232978de028aa5ce8f80
Ron-Chang <aaron909979@gmail.com>
2019-07-01 19:59:25 +0800

    Add comments for OpenCV 02

--- Coding/0_System_Software/Sentdex/OpenCV/02_Interact_Webcam_Video/main.py
+++ Coding/0_System_Software/Sentdex/OpenCV/02_Interact_Webcam_Video/main.py
@@ -7,22 +7,34 @@ cap = cv2.VideoCapture(0)
 # 1 - Second etc.
 
 # declare a codec
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+fourcc = cv2.VideoWriter_fourcc('j', 'p', 'e', 'g')
+# note the lower case
 
-out = cv2.VideoWriter("output.mov", fourcc, 24, (1280,720),True)
+# Get the camera properties
+# https://blog.csdn.net/tuzixini/article/details/78847942
+fps = cap.get(cv2.CAP_PROP_FPS)
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+# (w=1280, h=720) == (16:9)
 
-
+# create a writer
+out = cv2.VideoWriter("output.mov", fourcc, fps, size)
 
 # Infinite Loop
 while True:
     ret, frame = cap.read()
     # ret - return True or False
 
+    # Convert BGR to Grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Write frame into out
     out.write(frame)
 
-    cv2.imshow("Gray", gray)
+    # Display Grayscale version
+    cv2.imshow("GRAY", gray)
 
+    # Terminate key binding
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
